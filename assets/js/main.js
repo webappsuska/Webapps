77704ae4b7f9fe2c36ea65141e21b826a0d1087e
WebApp Suska <120287605+webappsuska@users.noreply.github.com>
2022-12-12 02:21:25 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,3 +234,21 @@
   });
 
 })()
+
+
+function gotoWhatsapp(){
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  var url = "https://wa.me/6282172716552?text="
+  +"Name: "+name+"%0a"
+  +"Email: "+email+"%0a"
+  +"subject: "+subject+"%0a"
+  +"Pesan: "+message;
+
+  window.open(url,  '_blank').focus();
+}
+
+
